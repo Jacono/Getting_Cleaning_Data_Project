fc25c070af28ed78cd74ce243e8f2ef1a3787322
Jacono <marco.jacono@gmail.com>
2015-11-20 18:15:29 +0100

    Update run_analisys.R

--- run_analisys.R
+++ run_analisys.R
@@ -2,6 +2,9 @@
 # Getting and Cleaning Data
 # Coursera
 #
+# Author    : M.Jacono
+# Email     :marco.jacono@gmail.com
+# 
 # You should create one R script called run_analysis.R that does the following. 
 # Merges the training and the test sets to create one data set.
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
@@ -27,18 +30,17 @@ unzip(destfile)
 
 
 # 1. Merge the training and the test sets to create one data set.
-
 setwd("./UCI HAR Dataset/");
 
 features     = read.table('./features.txt',header=FALSE) #imports features.txt
-activityType = read.table('./activity_labels.txt',header=FALSE) #imports activity_labels.txt
+activityLabels = read.table('./activity_labels.txt',header=FALSE) #imports activity_labels.txt
 
 # Import Training DataSet
-subjectTrain = read.table('./train/subject_train.txt',header=FALSE) #imports subject_train.txt
-xTrain       = read.table('./train/x_train.txt',header=FALSE) #imports x_train.txt
-yTrain       = read.table('./train/y_train.txt',header=FALSE) #imports y_train.txt
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE) 
+xTrain       = read.table('./train/x_train.txt',header=FALSE) 
+yTrain       = read.table('./train/y_train.txt',header=FALSE) 
 
-# Assigin column names to the data imported above
+# Change column name
 colnames(activityType)  = c('activityId','activityType')
 colnames(subjectTrain)  = "subjectId"
 colnames(xTrain)        = features[,2]
@@ -120,3 +122,55 @@ tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE);
 
 # Export the tidyData set 
 write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t');
+
+
+
+
+setwd("~/datascience/3. Obtaining Data/Project/")
+training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
+# Read features and make the feature names better suited for R with some substitutions
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Merge training and test sets together
+allData = rbind(training, testing)
+
+# Get only the data on mean and std. dev.
+colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
+# First reduce the features table to what we want
+features <- features[colsWeWant,]
+# Now add the last two columns (subject and activity)
+colsWeWant <- c(colsWeWant, 562, 563)
+# And remove the unwanted columns from allData
+allData <- allData[,colsWeWant]
+# Add the column names (features) to allData
+colnames(allData) <- c(features$V2, "Activity", "Subject")
+colnames(allData) <- tolower(colnames(allData))
+
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+  allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
+  currentActivity <- currentActivity + 1
+}
+
+allData$activity <- as.factor(allData$activity)
+allData$subject <- as.factor(allData$subject)
+
+tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
+# Remove the subject and activity column, since a mean of those has no use
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, "tidy.txt", sep="\t")
+
+
